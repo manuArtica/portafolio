8729792819655d453839ae930d5e83bf086b69e7
manu <aceptoelreto27@gmail.com>
2020-08-01 13:03:09 +0200

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,13 +10,28 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
-  constructor( private http: HttpClient ) {
+  equipo: any[] = [];
 
-    this.http.get('assets/data/data-pagina.json').subscribe( (resp: InfoPagina) => {
+  constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo() {
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
+    });
+   }
+
+   private cargarEquipo() {
+    this.http.get('https://angular-html-efa30.firebaseio.com/equipo.json')
+    .subscribe( ( resp: any ) => {
+
+      this.equipo = resp;
     });
    }
 }
